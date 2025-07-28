c854fcd9290f4464d466a3c8bc8f388edc13d4c2
Michael-Aladejobi <mikkystechs@gmail.com>
2025-07-28 19:32:13 +0100

    ...

--- index.ts
+++ index.ts
@@ -1,3 +1,45 @@
-let myName: string = "Mike";
-myName = 'kate';
-console.log(myName);
+// // Strings
+// let myName: string = "Mike";
+// myName = "some other person";
+// console.log(myName);
+
+// // Numbers
+// let favNumber: number = 8;
+// console.log(favNumber);
+
+// // Boolean
+// let isAlive: boolean = true
+// console.log(isAlive)
+
+// let tech = "TypeScript";
+// // tech = 12 //Error
+// console.log(typeof tech);
+
+// let color: any = "TypeScript";
+// color = 12;
+// color = true;
+// console.log(typeof color);
+
+// Regular Function
+
+// function addOne(num: number) {
+//   return num + 1;
+// }
+
+// console.log(addOne(3));
+
+// // Arrow Function
+// const double = (x: number, y: number) => {
+//   return x * y;
+// };
+// console.log(double(2, 2));
+
+// function greet(person: string = "Anonymous") {
+//   return ` Hello, ${person}!`;
+// }
+
+// console.log(greet());
+// console.log(greet("Mike"));
+
+
+
