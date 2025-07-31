b2478f41ad6bcd612a17327263a41383c8764f8d
Michael-Aladejobi <mikkystechs@gmail.com>
2025-07-31 13:11:05 +0100

    ...

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-const arr1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const arr2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-const O2 = (arrX, arrY) => {
-  for (let i = 0; i < arrX.length; i++) {
-    for (let j = 0; j < arrY.length; j++) {
-      console.log(`${i} x ${j} = ${i * j}`);
-    }
-  }
-};
-
-const res = O2(arr1, arr2);
-
 // const data = ["john", "kate", "smith", "tope", "mike"];
 
 // const fetchStudent = (arr, item) => {
@@ -36,3 +23,15 @@ const res = O2(arr1, arr2);
 // console.log(res);
 
 // 0(N x N)
+const arr1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const arr2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const O2 = (arrX, arrY) => {
+  for (let i = 0; i < arrX.length; i++) {
+    for (let j = 0; j < arrY.length; j++) {
+      console.log(`${i} x ${j} = ${i * j}`);
+    }
+  }
+};
+
+const res = O2(arr1, arr2)
